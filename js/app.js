b6478c9277317361c2de5f8fea584f600c9d3eb9
Sushant Kamkar <skkamkar@amazon.com>
2019-03-01 16:16:15 -0800

    Blank files

---
+++ js/app.js
@@ -0,0 +1,12 @@
+body {
+    margin: 0px;
+    behavior: url(csshover.htc);
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 80%;
+    color: #666666;
+    font-weight: normal;
+    text-decoration: none;
+    line-height: 1.4;
+    letter-spacing: 1px;
+    background-color: #D9382A;
+};
